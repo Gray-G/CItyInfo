ed749122c760394bb07bc8cb5311cf5826fbdf28
Gray-G <graygolden@gmail.com>
2017-06-30 12:21:43 -0700

    Add dependency injection of ILogger to PointsOfInterestController.cs
    Injects ILogger into PointsOfInterestController.cs's constructor and implements it within the GetPointsOfInterest method. Adds a try-catch block to return status code 500 with a message, in case of critical error.

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -38,6 +38,8 @@ namespace CityInfo.API
         {
             loggerFactory.AddConsole();
 
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
